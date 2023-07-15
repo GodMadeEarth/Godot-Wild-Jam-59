879bb3de0855f52ca187149f957cc8b126b62b01
GodMadeEarth <88457199+GodMadeEarth@users.noreply.github.com>
2023-07-15 15:14:24 -0400

    Train Improv

--- Godot-wild-jam-59/Train.gd
+++ Godot-wild-jam-59/Train.gd
@@ -10,7 +10,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	move(delta)
 
 func move(delta):
