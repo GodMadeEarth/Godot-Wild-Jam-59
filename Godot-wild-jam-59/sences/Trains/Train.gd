ff7372038fad82303a07752c71c90080147c54d0
iceofspade <iceofspade@outlook.com>
2023-07-16 07:47:48 -0500

    Added the ability to add carts dynamically

--- Godot-wild-jam-59/sences/Trains/Train.gd
+++ Godot-wild-jam-59/sences/Trains/Train.gd
@@ -1,17 +1,22 @@
 extends Node2D
-
+@onready var train_cart = preload("res://sences/Trains/train_cart.tscn")
 @export var speed:int = 100
-var rotation_speed:float = PI/2
+var rotation_speed:float = PI*1.5
 var rotation_direction:= 0.0 : set = set_dir, get = get_dir
-
+@onready var last_cart = self
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	move_straight(delta)
+	if Input.is_action_just_pressed("ui_down"):
+		print("made cart")
+		add_cart()
+
 func move_arc(delta):
 	if Input.is_action_pressed("Move_L"):
 		set_dir(-0.05)
@@ -35,6 +40,19 @@ func move_straight(delta):
 	position += velocity
 
 
+func add_cart():
+	var cart = train_cart.instantiate()
+	var last_cart_joint = last_cart.get_node("PinJoint2D")
+
+	get_parent().add_child(cart)
+	cart.global_position = last_cart_joint.global_position
+	cart.rotation = last_cart.rotation
+	last_cart_joint.node_b = cart.get_path()
+	print(last_cart.rotation)
+	last_cart = cart
+	
+	print(last_cart.rotation)
+
 func set_dir(ajustment:float):
 	rotation_direction+=ajustment
 	if rotation_direction+ajustment > 1:
@@ -42,9 +60,5 @@ func set_dir(ajustment:float):
 	elif rotation_direction+ajustment < -1:
 		rotation_direction = -1
 		
-#	if rotation_direction+ajustment < 1 or rotation_direction+ajustment < -1:
-#		rotation_direction+=ajustment
-	print(rotation_direction)
-	
 func get_dir():
 	return rotation_direction
