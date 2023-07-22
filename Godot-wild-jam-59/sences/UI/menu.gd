d757bfbe97afcf9cde35e9473ef05af1902c5c83
iceofspade <iceofspade@outlook.com>
2023-07-21 21:47:49 -0500

    added art Icons, menu and settings

---
+++ Godot-wild-jam-59/sences/UI/menu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Settings.get_node("MarginContainer/VBoxContainer/exit btm").button_up.connect(_on_exit_btm_up)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+func _on_start_button_up():
+	get_tree().change_scene_to_file("res://sences/game.tscn")
+	pass # Replace with function body.
+
+
+func _on_settings_button_up():
+	$Settings.visible = true
+	$"menu btn container/quit".disabled = true
+	$"menu btn container/start".disabled = true
+	pass # Replace with function body.
+
+
+func _on_quit_button_up():
+	get_tree().quit()
+	pass # Replace with function body.
+
+func _on_exit_btm_up():
+	$"menu btn container/quit".disabled = false
+	$"menu btn container/start".disabled = false
---
+++ Godot-wild-jam-59/sences/UI/settings.gd
@@ -0,0 +1,34 @@
+extends Control
+
+@onready var music_text = $MarginContainer/VBoxContainer/Music/music
+@onready var music_slider = $"MarginContainer/VBoxContainer/Music/music slider"
+@onready var sound_text = $MarginContainer/VBoxContainer/sound/sound
+@onready var sound_slider = $"MarginContainer/VBoxContainer/sound/sound slider"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	music_text.text = "Music: "+str(music_slider.value)
+	sound_text.text = "Sound: "+str(sound_slider.value)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	pass
+
+
+func _on_music_slider_value_changed(value):
+	music_text.text = "Music: "+str(value)
+	print(value)
+	pass # Replace with function body.
+
+
+func _on_sound_slider_value_changed(value):
+	sound_text.text = "Sound: "+str(value)
+	print(value)
+	pass # Replace with function body.
+
+
+func _on_exit_btm_button_up():
+	visible = false
+	pass # Replace with function body.
